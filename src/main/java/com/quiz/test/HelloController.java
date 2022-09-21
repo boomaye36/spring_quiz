d703b02a765e9801f556ada0b529f02db87fbe92
DESKTOP-KHVJGSC\user <0414soyeon@gmail.com>
2022-09-21 19:09:07 +0900

    921

---
+++ src/main/java/com/quiz/test/HelloController.java
@@ -0,0 +1,17 @@
+package com.quiz.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller 
+public class HelloController {
+
+	// http://localhost:8080/test1
+	@ResponseBody
+	@RequestMapping("/test2")
+	public String test1() {
+		return "Hello world!!!";
+	}
+	
+}
